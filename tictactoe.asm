01f195569281c5de1a70d3dd238e4a75660680b9
OliverKillane <oliverkillane@gmail.com>
2021-01-26 22:01:34 +0000

    now it runs

--- tictactoe.asm
+++ tictactoe.asm
@@ -1,8 +1,12 @@
 .data
-cells: .ascii "123456789"
-str1: .asciiz "Welcome to tic tac toe"
+cells: .ascii "         "
+str1: .asciiz "Welcome to tic tac toe: "
 str2: .asciiz "The board is:\n"
-str3: .asciiz "The winner is"
+str3: .asciiz "The winner is: "
+str4: .asciiz "Choose position: "
+str5: .asciiz "Oopsie, invalid input"
+p1: .byte 'X'
+p2: .byte 'O'
 .text
 
 
@@ -11,11 +15,28 @@ main:
     li $v0, 4
     la $a0, str1
     syscall
+
+gloop:
     jal print
 
     li $v0, 4
     la $a0, str3
     syscall
+
+    lb $t1, p1
+    jal choose
+
+    jal print
+
+    li $v0, 4
+    la $a0, str3
+    syscall
+
+    lb $t1, p2
+    jal choose
+
+
+    b gloop
 
     b exit
 
@@ -50,6 +71,30 @@ continue:
     jr $ra
 
 # Get user move choice
+
+error:
+    li $v0, 4
+    la $a0, str5
+    syscall
+choose:
+    #inefficient, resetting of constants, fix somewhere.
+    li $v0, 4
+    la $a0, str4
+    syscall
+    li $v0, 5
+    syscall
+
+    li $t2, 8
+    bgt $v0, $t2, error
+    blt $v0, $0, error
+    
+    lb $t3, cells($v0)
+    li $t4, ' '
+    bne $t3, $t4, error
+
+    sb $t1, cells($v0)
+    jr $ra
+
 
 
 
